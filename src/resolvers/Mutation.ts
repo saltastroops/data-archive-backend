b45b697f8e06da3ab074dca761d12133fd70ad3d
Christian Hettlage <hettlage@saao.ac.za>
2019-01-31 18:04:55 +0200

    removed query and mutation

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -1,13 +1,6 @@
 import { Prisma } from "../generated/prisma-client";
 
 // Defining  Mutation methods
-const Mutation = {
-  // Mutation for creating the user
-  async addUser(root: any, args: any, ctx: { prisma: Prisma }) {
-    return ctx.prisma.createUser({
-      name: args.name
-    });
-  }
-};
+const Mutation = {};
 
 export { Mutation };
--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,15 +1,6 @@
 import { Prisma } from "../generated/prisma-client";
 
 // Defining Query methods
-const Query = {
-  // Query for users
-  users(root: any, args: any, ctx: { prisma: Prisma }) {
-    const limit = args.limit ? Math.min(args.limit, 5) : 5;
-    return ctx.prisma.users({
-      first: limit,
-      orderBy: "createdAt_DESC"
-    });
-  }
-};
+const Query = {};
 
 export { Query };
