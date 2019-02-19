79dcca2b9aefd8db20e50f08b613930ab2ee8153
Christian Hettlage <hettlage@saao.ac.za>
2019-02-19 23:50:24 +0200

    fixed unit tests

--- src/__tests__/userRegistration.spec.ts
+++ src/__tests__/userRegistration.spec.ts
@@ -1,29 +1,17 @@
 jest.mock("../generated/prisma-client");
 
 import bcrypt from "bcrypt";
-import { prisma, UserWhereInput } from "../generated/prisma-client";
+import {
+  prisma,
+  UserCreateInput,
+  UserWhereUniqueInput
+} from "../generated/prisma-client";
 import { resolvers } from "../resolvers";
 
 beforeAll(() => {
-  // Mocking the user query
-  (prisma.user as any).mockImplementation(async (where: UserWhereInput) => {
-    if (where.username === "test" || where.email === "test@gmail.com") {
-      return {
-        affiliation: "Test Affiliation",
-        email: "test@gmail.com",
-        familyName: "Test",
-        givenName: "Test",
-        id: "1",
-        password: "hashedpassword",
-        roles: [],
-        username: "test"
-      };
-    }
-  });
-
   // Mocking the createUser mutation
-  (prisma.createUser as any).mockImplementation(
-    async (data: UserWhereInput) => ({
+  (prisma.createUser as any).mockImplementation((data: UserCreateInput) =>
+    Promise.resolve({
       affiliation: data.affiliation,
       email: data.email,
       familyName: data.familyName,
@@ -37,12 +25,17 @@ beforeAll(() => {
 
 afterAll(() => {
   // Cleaning up
-  (prisma.user as any).mockReset();
+  (prisma.users as any).mockReset();
   (prisma.createUser as any).mockReset();
 });
 
-describe("User registered", () => {
-  it("should register the user successfuly", async () => {
+describe("User registration", () => {
+  afterEach(() => {
+    // Cleaning up
+    (prisma.users as any).mockReset();
+  });
+
+  it("should register the user successfully", async () => {
     // User signing up with valid information.
     const args = {
       affiliation: "Test1 Affiliation",
@@ -53,7 +46,27 @@ describe("User registered", () => {
       username: "test1"
     };
 
-    const response = await resolvers.Mutation.signup({}, args, {
+    // Mock the users query. For the first two calls an empty array is returned
+    // (the user does not exist yet), for the third call an array with the new
+    // user is returned.
+    (prisma.users as any)
+      .mockResolvedValueOnce([])
+      .mockResolvedValueOnce([])
+      .mockResolvedValueOnce([
+        {
+          affiliation: "Test Affiliation",
+          email: "test@gmail.com",
+          familyName: "Test",
+          givenName: "Test",
+          id: "1",
+          password: "hashedpassword",
+          roles: [],
+          username: "test"
+        }
+      ]);
+
+    // Register the user
+    await resolvers.Mutation.signup({}, args, {
       prisma,
       user: { id: "" }
     });
@@ -61,25 +74,30 @@ describe("User registered", () => {
     // Expect createUser to have been called
     expect(prisma.createUser).toHaveBeenCalled();
 
-    const argumentsWithoutPassword = delete { ...args }.password;
+    // createUser should have been called with the submitted arguments, but
+    // the password shou,d have been hashed. So for comparison purposes we
+    // need the arguments without the password.
+    const argumentsWithoutPassword = { ...args };
+    delete argumentsWithoutPassword.password;
 
-    const storedDataWithoutPassword = delete {
+    const storedDataWithoutPassword = {
       ...(prisma.createUser as any).mock.calls[0][0]
-    }.password;
+    };
+    delete storedDataWithoutPassword.password;
 
-    // Expect the submitted user information to have been stored in the database.
-    expect(storedDataWithoutPassword).toBe(argumentsWithoutPassword);
+    // Expect the submitted user information to have been stored in the
+    // database.
+    expect(storedDataWithoutPassword).toEqual(argumentsWithoutPassword);
 
-    // Expect the submitted user password stored in the database to have been hashed.
+    // Expect the submitted user password stored in the database to have been
+    // hashed.
     expect((prisma.createUser as any).mock.calls[0][0].password).not.toBe(
       args.password
     );
   });
-});
 
-describe("User not registered", () => {
-  it("should not register the user submitted a username containing an uppercase character", async () => {
-    // User signing up with a username that contains an uppercased character(s).
+  it("should not register the user with a username containing an upper case character", async () => {
+    // User signing up with a username that contains an upper case character
     const args = {
       affiliation: "Test2 Affiliation",
       email: "test2@gmail.com",
@@ -89,58 +107,84 @@ describe("User not registered", () => {
       username: "tesT2"
     };
 
-    const response = await resolvers.Mutation.signup({}, args, {
-      prisma,
-      user: { id: "" }
-    });
+    // Mock the users query with an empty array (the user does not exist yet)
+    (prisma.users as any).mockResolvedValue([]);
 
-    // Expect the error message to be meaningful.
-    expect(response.message).toContain("tesT2");
+    // Expect signing up to fail with the appropriate error
+    let message = "";
+    try {
+      await resolvers.Mutation.signup({}, args, {
+        prisma,
+        user: { id: "" }
+      });
+    } catch (e) {
+      message = e.message;
+    }
+    expect(message).toContain("tesT2");
   });
 
-  it("should not register the user with an existing username", async () => {
-    // User signing up with a username that already exists.
+  it("should not register a user with an existing username", async () => {
+    // User signing up with a username that already exists
     const args = {
       affiliation: "Test3 Affiliation",
       email: "test3@gmail.com",
       familyName: "Test3",
       givenName: "Test3",
       password: "test3",
-      username: "test"
+      username: "existingusername"
     };
 
-    const response = await resolvers.Mutation.signup({}, args, {
-      prisma,
-      user: { id: "" }
-    });
+    // Mock the users query. A non-empty array is returned (the user exists
+    // already).
+    (prisma.users as any).mockResolvedValue([{ id: "42" }]);
 
-    // Expect the error message to be meaningful.
-    expect(response.message).toContain("test");
-    expect(response.message).toContain("exists");
+    // Expect signing up to fail with the appropriate error.
+    let message = "";
+    try {
+      await resolvers.Mutation.signup({}, args, {
+        prisma,
+        user: { id: "" }
+      });
+    } catch (e) {
+      message = e.message;
+    }
+    expect(message).toContain("exists");
+    expect(message).toContain("existingusername");
   });
 
-  it("should not register the user with an existing email address", async () => {
-    // User signing up with an email address that already exists.
+  it("should not register a user with an existing email address", async () => {
+    // User signing up with an email address that already exists
     const args = {
       affiliation: "Test4 Affiliation",
-      email: "test@gmail.com",
+      email: "existing@gmail.com",
       familyName: "Test4",
       givenName: "Test4",
       password: "test4",
       username: "test4"
     };
 
-    const response = await resolvers.Mutation.signup({}, args, {
-      prisma,
-      user: { id: "" }
-    });
+    // Mock the users query. The first call returns an empty array (there is no
+    // user with the given username yet), the second call returns a non-empty
+    // array (there exists a user with the email address already).
+    (prisma.users as any)
+      .mockResolvedValueOnce([])
+      .mockResolvedValue([{ id: 7 }]);
 
-    // Expect the error message to be meaningful.
-    expect(response.message).toContain("test@gmail.com");
-    expect(response.message).toContain("exists");
+    // Expect signing up to fail with the appropriate error.
+    let message = null;
+    try {
+      await resolvers.Mutation.signup({}, args, {
+        prisma,
+        user: { id: "" }
+      });
+    } catch (e) {
+      message = e.message;
+    }
+    expect(message).toContain("exists");
+    expect(message).toContain("existing@gmail.com");
   });
 
-  it("should not register the user submitted a password with less than 6 characters", async () => {
+  it("should not register the user with a password shorter than 7 characters", async () => {
     // User signing up with the password less than 6 characters.
     const args = {
       affiliation: "Test5 Affiliation",
@@ -151,12 +195,19 @@ describe("User not registered", () => {
       username: "test5"
     };
 
-    const response = await resolvers.Mutation.signup({}, args, {
-      prisma,
-      user: { id: "" }
-    });
+    // Mock the users query with an empty array (the user does not exist yet)
+    (prisma.users as any).mockResolvedValue([]);
 
-    // Expect the error message to be meaningful.
-    expect(response.message).toContain("6 characters long");
+    // Expect signing up to fail with the appropriate error.
+    let message = "";
+    try {
+      await resolvers.Mutation.signup({}, args, {
+        prisma,
+        user: { id: "" }
+      });
+    } catch (e) {
+      message = e.message;
+    }
+    expect(message).toContain("at least 7 characters long");
   });
 });
