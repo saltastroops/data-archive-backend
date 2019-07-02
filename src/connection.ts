f2d95e2f2b3ada179d71263b9c99dd3cd7889654
Eb-Zeero <macebelene@gmail.com>
2019-07-02 09:13:46 +0200

    Database connection

---
+++ src/connection.ts
@@ -0,0 +1,38 @@
+import mysql from 'mysql'
+
+
+class Database {
+    public connection: any;
+
+    private config = {
+        database : process.env.DATABASE_NAME,
+        host     : process.env.DATABASE_HOST,
+        password : process.env.DATABASE_PASSWORD,
+        user     : process.env.DATABASE_USER
+    };
+
+    constructor() {
+        this.connection = mysql.createConnection( this.config );
+    }
+    query( sql: any, args: any ): any {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err: any, rows: any ) => {
+                if ( err ) {
+                    return reject(err);
+                }
+                resolve( rows );
+            } );
+        } );
+    }
+    public close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( (err: any) => {
+                if ( err ) {
+                    return reject(err);
+                }
+                resolve();
+            } );
+        } );
+    }
+}
+export default Database;
