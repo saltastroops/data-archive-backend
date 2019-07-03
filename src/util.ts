b3f0699cd1138cc12267948a54a64bc68e315a0f
Sifiso Myeza <myezasifiso11@gmail.com>
2019-07-03 10:34:54 +0200

    Format fix

--- src/util.ts
+++ src/util.ts
@@ -11,13 +11,13 @@ interface IUser {
 }
 
 const transporter = nodemailer.createTransport({
-    auth: {
-        pass: process.env.MAIL_PASSWORD,
-        user: process.env.MAIL_USER,
-    },
+  auth: {
+    pass: process.env.MAIL_PASSWORD,
+    user: process.env.MAIL_USER
+  },
   host: process.env.MAIL_HOST,
   port: 465,
   secure: true
 });
 
-export {IContext, transporter };
+export { IContext, transporter };
