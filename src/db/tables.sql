8147a8978ad28ce5d703b797977dcf16dd61c42c
Christian Hettlage <hettlage@saao.ac.za>
2019-07-18 11:43:06 +0200

    language changes; use unsigned int; two new tables

--- src/db/tables.sql
+++ src/db/tables.sql
@@ -1,74 +1,110 @@
 use `ssda-prisma`;
 SET FOREIGN_KEY_CHECKS=0;
+
 --
--- Table structure for table `Role`
+-- User roles
 --
 
 DROP TABLE IF EXISTS `Role`;
+
 CREATE TABLE `Role` (
-  `roleId` INT(11) NOT NULL AUTO_INCREMENT COMMENT "Table's Id",
-  `role` VARCHAR(45) NOT NULL COMMENT "Role for the user e.g ADMIN",
+  `roleId` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT "Primary key",
+  `role` VARCHAR(45) NOT NULL COMMENT "Role for the user, e.g ADMIN",
   PRIMARY KEY (`roleId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `Role` (`roleId`, `role`) values (1, "ADMIN");
+INSERT INTO `Role` (`roleId`, `role`) VALUES (1, "ADMIN");
 
 --
--- Table structure for table `DataRequestStatus`
+-- Data request status values
 --
 
 DROP TABLE IF EXISTS `DataRequestStatus`;
+
 CREATE TABLE `DataRequestStatus` (
-  `dataRequestStatusId` INT(11) NOT NULL AUTO_INCREMENT COMMENT "Table's Id",
-  `dataRequestStatus` VARCHAR(45) NOT NULL COMMENT "A Data request status e.g SUCCESSFUL",
+  `dataRequestStatusId` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT "Primary key",
+  `dataRequestStatus` VARCHAR(45) NOT NULL COMMENT "A Data request status, e.g SUCCESSFUL",
   PRIMARY KEY (`dataRequestStatusId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-INSERT INTO `DataRequestStatus` (`dataRequestStatus`) values ("SUCCESSFUL"), ("PENDING"), ("FAILED");
+
+INSERT INTO `DataRequestStatus` (`dataRequestStatus`) VALUES ("SUCCESSFUL"), ("PENDING"), ("FAILED");
 
 --
--- Table structure for table `User`
+-- User
 --
+
 DROP TABLE IF EXISTS `User`;
+
 CREATE TABLE `User` (
-  `userId` INT(11) NOT NULL AUTO_INCREMENT,
+  `userId` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `familyName` VARCHAR(255) NOT NULL COMMENT "Family name (surname)",
   `givenName` VARCHAR(255) NOT NULL COMMENT "Given name (first name)",
   `username` VARCHAR(255) NOT NULL UNIQUE COMMENT "Username, which must not contain upper case letters",
   `email` VARCHAR(255) NOT NULL UNIQUE COMMENT "Email address",
   `affiliation` VARCHAR(255) NOT NULL COMMENT "Affiliation, such as a university or an institute",
   `password` VARCHAR(255) NOT NULL COMMENT "Password, which must have at least 7 characters",
-  `passwordResetToken` VARCHAR(255) UNIQUE COMMENT "A Token to reset a password",
-  `passwordResetTokenExpiry` VARCHAR(255) COMMENT "Date a tocken will expire",
+  `passwordResetToken` VARCHAR(255) UNIQUE COMMENT "Token to reset the user's password",
+  `passwordResetTokenExpiry` DATETIME COMMENT "Time when the password reset token will expire",
   PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Table structure for table `UserRole`
--- A single user can have multiple roles
+-- Remote account which is for a user of this database, e.g. an account in the SALT Science Database.
+--
+
+DROP TABLE IF EXISTS `RemoteUserAccount`;
+
+CREATE TABLE `RemoteUserAccount` (
+    `userId` INT(11) UNSIGNED NOT NULL COMMENT "User id for this database",
+    `remoteUserId` VARCHAR(255) NOT NULL COMMENT "User id for the remote account",
+    `remoteInstitutionId` INT(11) UNSIGNED NOT NULL COMMENT "Id of the remote institution",
+    PRIMARY KEY (`userId`, `remoteUserId`, `remoteInstitutionId`),
+    KEY `fk_RemoteUserAccountUser_idx` (`userId`),
+    KEY `fk_RemoteUserAccountRemoteInstitution_idx` (`remoteInstitutionId`),
+    CONSTRAINT `fk_RemoteUserAccountUser` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `fk_RemoteUserAccountRemoteInstitution` FOREIGN KEY (`remoteInstitutionId`) REFERENCES `Institution` (`institutionId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- An institution to whose accounts a user can be linked
+--
+
+DROP TABLE IF EXISTS `Institution`;
+
+CREATE TABLE `Institution` (
+    `institutionId` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT "Primary key",
+    `institution` VARCHAR(255) NOT NULL COMMENT "Name of the institution",
+    PRIMARY KEY (`institutionId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `Institution` (`institution`) VALUES ("SAAO"), ("SALT");
+
+--
+-- User role. A user can have multiple roles.
 --
 DROP TABLE IF EXISTS `UserRole`;
+
 CREATE TABLE `UserRole` (
-  `userId` INT(11) NOT NULL COMMENT "An id of a table `User`",
-  `roleId` INT(11) NOT NULL COMMENT "An id of a table `Role`",
+  `userId` INT(11) UNSIGNED NOT NULL COMMENT "User id`",
+  `roleId` INT(11) UNSIGNED NOT NULL COMMENT "User role ide",
   PRIMARY KEY (`userId`, `roleId`),
   KEY `fk_UserRoleUser_idx` (`userId`),
   KEY `fk_UserRoleRole_idx` (`roleId`),
   CONSTRAINT `fk_UserRoleUser` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_UserRoleRole` FOREIGN KEY (`roleId`) REFERENCES `Tole` (`roleId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_UserRoleRole` FOREIGN KEY (`roleId`) REFERENCES `Role` (`roleId`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 --
--- Table structure for table `DataRequest`
+-- Data request
 --
 
 DROP TABLE IF EXISTS `DataRequest`;
+
 CREATE TABLE `DataRequest` (
-  `dataRequestId` INT(11) NOT NULL AUTO_INCREMENT COMMENT "Table's id",
-  `userId` INT(11) NOT NULL COMMENT "An id for a table `User`",
-  `dataRequestStatusId` INT(11) NOT NULL COMMENT "An id for a table `DataRequestStatus`",
-  `dataRequest` VARCHAR(255) NOT NULL COMMENT "@Christian do we have a name for data request if not please remove",
-  `madeAt` DATETIME NOT NULL COMMENT "Date and time a data request is made",
+  `dataRequestId` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT "Primary key",
+  `userId` INT(11) UNSIGNED NOT NULL COMMENT "User id",
+  `dataRequestStatusId` INT(11) UNSIGNED NOT NULL COMMENT "Data requrest status id",
+  `madeAt` DATETIME NOT NULL COMMENT "Time when the request was made",
   PRIMARY KEY (`dataRequestId`),
   KEY `fk_DataRequestUser_idx` (`userId`),
   KEY `fk_DataRequestStatus_idx` (`dataRequestStatusId`),
@@ -78,16 +114,18 @@ CREATE TABLE `DataRequest` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Table structure for table `DataRequestFiles`
+-- File in a data request.
 --
 
-DROP TABLE IF EXISTS `DataRequestFiles`;
-CREATE TABLE `DataRequestFiles` (
-  `dataRequestId` INT(11) NOT NULL  COMMENT "An Id for a table ``DataRequest",
-  `fileId` INT(11) NOT NULL COMMENT "An id from SSDA for a data file this need to be checked before it is created",
+DROP TABLE IF EXISTS `DataRequestFile`;
+
+CREATE TABLE `DataRequestFile` (
+  `dataRequestId` INT(11) UNSIGNED NOT NULL  COMMENT "An Id for a table ``DataRequest",
+  `fileId` INT(11) UNSIGNED NOT NULL COMMENT "An id from SSDA for a data file this need to be checked before it is created",
   PRIMARY KEY (`dataRequestId`,`fileId` ),
   KEY `fk_DataRequestFileDataRequest_idx` (`dataRequestId`),
   CONSTRAINT `fk_DataRequestFilesDataRequest` FOREIGN KEY (`dataRequestId`) REFERENCES `DataRequest` (`dataRequestId`) ON DELETE NO ACTION ON UPDATE NO ACTION
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 SET FOREIGN_KEY_CHECKS=1;
