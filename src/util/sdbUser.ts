403e9ffe64019b15ca06e423a5f0385fb1727709
Eb-Zeero <macebelene@gmail.com>
2019-07-23 17:21:20 +0200

    sdb user get used by id and get login user

---
+++ src/util/sdbUser.ts
@@ -0,0 +1,41 @@
+import { sdbPool } from "../db/pool";
+
+export const getSaltUserById = async (userId: string) => {
+  const user: any = await sdbPool.query(
+    `
+SELECT * FROM PiptUser JOIN Investigator USING(Investigator_Id) WHERE PiptUser.PiptUser_Id=?
+      `,
+    [userId]
+  );
+  return user[0]
+    ? {
+        affiliation: "SALT",
+        email: user[0].Email,
+        familyName: user[0].Surname,
+        givenName: user[0].FirstName,
+        id: user[0].PiptUser_Id,
+        roles: [],
+        username: user[0].Username
+      }
+    : undefined;
+};
+
+export const loginSaltUser = async (username: string, password: string) => {
+  const user: any = await sdbPool.query(
+    `
+SELECT * FROM PiptUser JOIN Investigator USING(Investigator_Id) WHERE Username=? AND Password=MD5(?);
+      `,
+    [username, password]
+  );
+  return user[0]
+    ? {
+        affiliation: "SALT",
+        email: user[0].Email,
+        familyName: user[0].Surname,
+        givenName: user[0].FirstName,
+        id: user[0].PiptUser_Id,
+        roles: [],
+        username: user[0].Username
+      }
+    : undefined;
+};
