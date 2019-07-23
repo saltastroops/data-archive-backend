dc90953e0aa4101825f9feb56e8d1656a5bbf277
Eb-Zeero <macebelene@gmail.com>
2019-07-23 17:19:15 +0200

    SDB pool

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -9,8 +9,17 @@ const config = {
   user: process.env.DATABASE_USER
 };
 
+// SDB configuration parameters
+const sdbConfig = {
+  database: process.env.SDB_DATABASE_NAME,
+  host: process.env.SDB_DATABASE_HOST,
+  password: process.env.SDB_DATABASE_PASSWORD,
+  user: process.env.SDB_DATABASE_USER
+};
+
 // Creating a pool of database connections
 const pool = mysql.createPool(config);
+const sdbPool = mysql.createPool(sdbConfig);
 
 // Establishing the database connection
 pool.getConnection((err, connection) => {
@@ -26,4 +35,19 @@ pool.getConnection((err, connection) => {
 // Promisifying the query function so that async / wait can be used
 (pool.query as any) = util.promisify(pool.query);
 
+// Establishing the sdb connection
+sdbPool.getConnection((err, connection) => {
+  if (err) {
+    throw Error("Something went wrong, please try again later.");
+  }
+  if (connection) {
+    connection.release();
+  }
+  return;
+});
+
+// Promisifying the query function so that async / wait can be used
+(sdbPool.query as any) = util.promisify(sdbPool.query);
+
 export default pool;
+export { sdbPool };
