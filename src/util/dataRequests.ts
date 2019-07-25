f5c0ba9a92c427d175beb18f2abce880b7b35883
Sifiso Myeza <myezasifiso11@gmail.com>
2019-07-25 08:23:21 +0200

    Add more details to the data request observation file from the datafile

--- src/util/dataRequests.ts
+++ src/util/dataRequests.ts
@@ -1,6 +1,6 @@
 import DataLoader from "dataloader";
 import { groupBy, map } from "ramda";
-import { ssdaAdminPool } from "../db/pool";
+import { ssdaAdminPool, ssdaPool } from "../db/pool";
 
 /**
  * A data request data loader function
@@ -29,24 +29,25 @@ const dataRequestFileDataLoader = () => {
  * @param userIds users information
  */
 const dataRequestsByUserIds = async (userIds: any) => {
+  // A select query for the user data request
   const sql = `
     SELECT dataRequestId AS id, dataRequestStatus AS status, madeAt, userId
     FROM DataRequest AS dr
     JOIN DataRequestStatus AS drs ON drs.dataRequestStatusId = dr.dataRequestStatusId
     WHERE dr.userId IN(?)
   `;
-  const params: any = [userIds];
-
-  const dataRequests: any = await ssdaAdminPool.query(sql, params);
-
+  // Retrieving user data requests
+  const dataRequests: any = await ssdaAdminPool.query(sql, [userIds]);
+  // A placeholder variable for data requests containing data request observations
   const dataRequestWithObservations: any = [];
-
+  // For each data reuest load it data request observations
   for (const dataRequest of dataRequests) {
     const observations: any = await dataRequestObservationDataLoader().load(
       dataRequest.id
     );
-
+    // A placeholder variable for data request observations constaining data request observation files
     const observationsWithFiles: any = [];
+    // For each data request observation, load id data request oobservation files
     for (const observation of observations) {
       const dataFiles: any = await dataRequestFileDataLoader().load(
         observation.id
@@ -60,14 +61,17 @@ const dataRequestsByUserIds = async (userIds: any) => {
     });
   }
 
+  // Group data request by user id
   const groupedById = groupBy((dataRequest: any) => {
     return dataRequest.userId;
   }, dataRequestWithObservations);
 
+  // Map data request to a belonging user
   const grouped = Object.keys(groupedById).length
     ? map((userId: any) => groupedById[userId], userIds)
     : [[]];
 
+  // return grouped user data requests
   return grouped;
 };
 
@@ -77,24 +81,27 @@ const dataRequestsByUserIds = async (userIds: any) => {
  * @param dataRequestIds data request ids
  */
 const dataRequestObservationByDataRequestIds = async (dataRequestIds: any) => {
+  // A select query for data request observations
   const sql = `
     SELECT dataRequestObservationId AS id, dataRequestId, name
     FROM DataRequestObservation AS dro
     WHERE dro.dataRequestId IN(?)
   `;
-  const params: any = [dataRequestIds];
-
-  const dataRequestObservations: any = await ssdaAdminPool.query(sql, params);
-
+  // Retrieving data request observations
+  const dataRequestObservations: any = await ssdaAdminPool.query(sql, [
+    dataRequestIds
+  ]);
+  // Group data request observations by data request id
   const groupedById = groupBy(
     (dataRequestObservation: any) => dataRequestObservation.dataRequestId,
     dataRequestObservations
   );
-
+  // Map data request observation to a belonging data request
   const grouped = Object.keys(groupedById).length
     ? map((dataRequestId: any) => groupedById[dataRequestId], dataRequestIds)
     : [[]];
 
+  // return grouped data requests observations
   return grouped;
 };
 
@@ -106,20 +113,39 @@ const dataRequestObservationByDataRequestIds = async (dataRequestIds: any) => {
 const dataRequestFilesByDataRequestObservationIds = async (
   dataRequestObservationIds: any
 ) => {
-  const sql = `
+  // A select query for data request observation files
+  let sql = `
     SELECT dataRequestFileId AS id, dataRequestObservationId, fileId, name
     FROM DataRequestFile AS drf
     WHERE drf.dataRequestObservationId IN(?)
   `;
-  const params: any = [dataRequestObservationIds];
+  // Retrieving data request observation files
+  const dataRequestFiles: any = await ssdaAdminPool.query(sql, [
+    dataRequestObservationIds
+  ]);
 
-  const dataRequestFiles: any = await ssdaAdminPool.query(sql, params);
+  // A placeholder variable to load more data file details
+  const dataRequestFilesWithDetails: any = [];
 
+  // For each data request file, get more data file information and add them
+  for (const dataRequestFile of dataRequestFiles) {
+    // A select query for data file
+    sql = `
+      SELECT *
+      FROM DataFile AS df
+      WHERE df.dataFileId = ?
+    `;
+    // A data file
+    const dataFile: any = await ssdaPool.query(sql, dataRequestFile.id);
+    // Loading more data file details
+    dataRequestFilesWithDetails.push({ ...dataRequestFile, ...dataFile[0] });
+  }
+  // Group data request observation files by data request observation id
   const groupedById = groupBy(
     (dataRequestFile: any) => dataRequestFile.dataRequestObservationId,
-    dataRequestFiles
+    dataRequestFilesWithDetails
   );
-
+  // Map data request observation files to a belonging data request observation
   const grouped = Object.keys(groupedById).length
     ? map(
         (dataRequestObservationId: any) =>
@@ -128,6 +154,7 @@ const dataRequestFilesByDataRequestObservationIds = async (
       )
     : [[]];
 
+  // return grouped data requests observation files
   return grouped;
 };
 
