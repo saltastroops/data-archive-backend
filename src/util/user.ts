8973ca8e2e11d1f562cfd70920721d9a4af57834
Christian Hettlage <hettlage@saao.ac.za>
2019-07-26 20:35:13 +0200

    moved validation into createUser function

--- src/util/user.ts
+++ src/util/user.ts
@@ -1,31 +1,125 @@
 import moment = require("moment");
 import { ssdaAdminPool } from "../db/pool";
+import bcrypt from "bcrypt";
+import { validate } from "isemail";
 
-export const createUser = async (args: any, hashedPassword: string) => {
-  // Query inserting a new user with with the supplied information
-  let sql = `
-    INSERT INTO User (affiliation, email, familyName, givenName, authProviderId, authProviderUserId) 
-    VALUES(?, ?, ?, ?, ?, ?)
+type AuthProvider = "SDB" | "SSDA";
+
+interface UserCreateInput {
+  affiliation: string;
+  authProvider: AuthProvider;
+  authProviderUserId?: string;
+  email: string;
+  familyName: string;
+  givenName: string;
+  password?: string;
+  username?: string;
+}
+
+/**
+ * Create a new user with the given details.
+ *
+ * The given password must not be encrypted. It must be sufficiently secure.
+ *
+ * An error is raised if a user with the given email address exists already.
+ */
+export const createUser = async (args: UserCreateInput) => {
+  const {
+    affiliation,
+    authProvider,
+    authProviderUserId,
+    email,
+    familyName,
+    givenName,
+    password,
+    username
+  } = args;
+
+  // Check if the submitted email address is valid
+  if (!validate(args.email, { minDomainAtoms: 2 })) {
+    throw new Error(`The email address "${args.email}" is invalid.`);
+  }
+
+  // Transform the email address to lower case.
+  const lowerCaseEmail = args.email.toLowerCase();
+
+  // Check if there already exists a user with the submitted email address
+  const userWithGivenEmail = await getUserByEmail(args.email);
+  if (userWithGivenEmail) {
+    throw new Error(
+      `There already exists a user with the email address ${args.email}.`
+    );
+  }
+
+  // The auth provider SSDA requires a password, and it must be secure; and it
+  // also required a valid username
+  if (authProvider === "SSDA") {
+    if (!password) {
+      throw new Error("The password is missing.");
+    }
+
+    checkPasswordStrength(password);
+
+    // Check that the submitted username is not empty
+    if (!args.username) {
+      throw new Error(`The username must not be empty.`);
+    }
+
+    // Check that the submitted username does not contain upper case characters
+    if (args.username !== args.username.toLowerCase()) {
+      throw new Error(
+        `The username ${args.username} contains upper case characters.`
+      );
+    }
+
+    // Check if there already exists a user with the submitted username
+    const userWithGivenUsername = await getUserByUsername(args.username);
+
+    if (userWithGivenUsername) {
+      throw new Error(
+        `There already exists a user with the username ${args.username}.`
+      );
+    }
+  }
+
+  // An auth provider other than SSDA requires the auth provider user id
+  if (authProvider !== "SSDA" && !authProviderUserId) {
+    throw new Error("The user id for the authentication provider is missing.");
+  }
+
+  // Insert the user with the given details into the database
+  const userInsertSQL = `
+      INSERT INTO User (affiliation, email, familyName, givenName, authProviderId, authProviderUserId)
+      VALUES (?, ?, ?, ?, ?, ?)
   `;
-
-  // Add the new user to the database
-  await ssdaAdminPool.query(sql, [
-    args.affiliation,
-    args.email,
-    args.familyName,
-    args.givenName,
-    1,
-    "SSDA"
+  await ssdaAdminPool.query(userInsertSQL, [
+    affiliation,
+    lowerCaseEmail,
+    familyName,
+    givenName,
+    authProvider !== "SSDA" ? authProvider : null,
+    authProvider !== "SSDA" ? authProviderUserId : null
   ]);
 
-  const { userId } = ((await ssdaAdminPool.query(
-    `SELECT MAX(userId) AS userId FROM User`
-  )) as any)[0];
+  // If SSDA is used as the auth provider, we have to store the user credentials
+  if (authProvider === "SSDA") {
+    // As the email is unique we can identify the new user by their email
+    const userIdSQL = `SELECT userId FROM User WHERE email=?`;
+    const { userId } = ((await ssdaAdminPool.query(userIdSQL)) as any)[0];
 
-  sql = `INSERT INTO SSDAUserAuth (userId, username, password) VALUES (?, ?, ?)`;
+    // Hash the password before storing it in the database
+    const hashedPassword = await bcrypt.hash(args.password, 10);
 
-  // Add the new user to the database
-  await ssdaAdminPool.query(sql, [userId, args.username, hashedPassword]);
+    // Store the user credentials
+    const authInsertSQL = `INSERT INTO SSDAUserAuth (userId, username, password) VALUES (?, ?, ?)`;
+
+    // Add the new user to the database
+    await ssdaAdminPool.query(userInsertSQL, [
+      userId,
+      args.username,
+      hashedPassword
+    ]);
+  }
 };
 
 export interface IUser {
@@ -218,3 +312,10 @@ export const ownsDataFile = (user: IUser | undefined, fileId: string) => false;
  */
 export const ownsDataRequest = (dataReqeust: any, user: IUser) =>
   dataReqeust.user.id === user.id;
+
+// Check whether a password is sufficiently strong.
+function checkPasswordStrength(password: string) {
+  if (password.length < 7) {
+    throw new Error(`The password must be at least 7 characters long.`);
+  }
+}
