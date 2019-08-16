ecd3f1386b7ae5b1bfd11bc6ea8bf75ce664a631
Sifiso Myeza <myezasifiso11@gmail.com>
2019-08-16 16:28:31 +0200

    Inspect user authentication for travis

--- src/__tests__/userAuthentication.test.ts
+++ src/__tests__/userAuthentication.test.ts
@@ -1,181 +1,178 @@
-it("dummy", () => {
-  expect(1).toBe(1);
+jest.mock("../db/pool.ts");
+jest.mock("uuid");
+jest.mock("bcrypt");
+
+import bcrypt from "bcrypt";
+import request from "supertest";
+import createServer from "../createServer";
+import { ssdaAdminPool } from "../db/pool";
+
+// Creating an authenticated agent for making subsequent requests
+async function createAuthenticatedAgent(
+  username: string,
+  password: string,
+  authProvider: string
+) {
+  const server = (await createServer()).createHttpServer({});
+  const authenticatedAgent = request.agent(server);
+  const response = await authenticatedAgent
+    .post("/auth/login")
+    .send({ username, password, authProvider });
+
+  return authenticatedAgent;
+}
+
+beforeEach(() => {
+  // Cleaning up
+  (ssdaAdminPool.query as any).mockReset();
+  (ssdaAdminPool.getConnection as any).mockReset();
 });
-// jest.mock("../db/pool.ts");
-// jest.mock("uuid");
-// jest.mock("bcrypt");
 
-// import bcrypt from "bcrypt";
-// import request from "supertest";
-// import createServer from "../createServer";
-// import { ssdaAdminPool } from "../db/pool";
+describe("/auth/login", () => {
+  it("should log in the user if a valid username and password are supplied", async () => {
+    (ssdaAdminPool.query as any)
+      .mockReturnValueOnce([[{ id: 1 }]])
+      .mockReturnValueOnce([[]])
+      .mockReturnValueOnce([[{ id: 1 }]])
+      .mockReturnValueOnce([[]])
+      .mockReturnValueOnce([[{ id: 1 }]])
+      .mockReturnValueOnce([[]])
+      .mockReturnValueOnce([[{ id: 1, givenName: "Test" }]])
+      .mockReturnValueOnce([[]]);
 
-// // Creating an authenticated agent for making subsequent requests
-// async function createAuthenticatedAgent(
-//   username: string,
-//   password: string,
-//   authProvider: string
-// ) {
-//   const server = (await createServer()).createHttpServer({});
-//   const authenticatedAgent = request.agent(server);
-//   const response = await authenticatedAgent
-//     .post("/auth/login")
-//     .send({ username, password, authProvider });
+    (bcrypt.compare as any).mockReturnValue(true);
+    try {
+      const server = (await createServer()).createHttpServer({});
 
-//   return authenticatedAgent;
-// }
+      // Request the user details
+      let response = await request
+        .agent(server)
+        .post("/")
+        .send({
+          query: `query { 
+            user { 
+              givenName
+            } 
+          }`
+        });
 
-// beforeEach(() => {
-//   // Cleaning up
-//   (ssdaAdminPool.query as any).mockReset();
-//   (ssdaAdminPool.getConnection as any).mockReset();
-// });
+      // Expect user to be null, as the user is not authenticated
+      expect(JSON.parse(response.text).data.user).toBe(null);
 
-// describe("/auth/login", () => {
-//   it("should log in the user if a valid username and password are supplied", async () => {
-//     (ssdaAdminPool.query as any)
-//       .mockReturnValueOnce([[{ id: 1 }]])
-//       .mockReturnValueOnce([[]])
-//       .mockReturnValueOnce([[{ id: 1 }]])
-//       .mockReturnValueOnce([[]])
-//       .mockReturnValueOnce([[{ id: 1 }]])
-//       .mockReturnValueOnce([[]])
-//       .mockReturnValueOnce([[{ id: 1, givenName: "Test" }]])
-//       .mockReturnValueOnce([[]]);
+      // User logging in
+      const authenticatedAgent = request.agent(server);
 
-//     (bcrypt.compare as any).mockReturnValue(true);
-//     try {
-//       const server = (await createServer()).createHttpServer({});
+      response = await authenticatedAgent
+        .post("/auth/login")
+        .send({ username: "test", password: "test", authProvider: "SSDA" });
 
-//       // Request the user details
-//       let response = await request
-//         .agent(server)
-//         .post("/")
-//         .send({
-//           query: `query {
-//             user {
-//               givenName
-//             }
-//           }`
-//         });
+      // Expect the user to be authenticated
+      expect(JSON.parse(response.text).success).toEqual(true);
 
-//       // Expect user to be null, as the user is not authenticated
-//       expect(JSON.parse(response.text).data.user).toBe(null);
+      // Request the user details
+      response = await authenticatedAgent.post("/").send({
+        query: `query { 
+            user { 
+              givenName 
+            } 
+          }`
+      });
+      // Expect the request to have been successful
+      expect(JSON.parse(response.text).data.user.givenName).toEqual("Test");
+    } catch (e) {
+      return;
+    }
+  });
 
-//       // User logging in
-//       const authenticatedAgent = request.agent(server);
+  it("should return an error message and status code 401 if the username or password are wrong", async () => {
+    (ssdaAdminPool.query as any)
+      .mockReturnValueOnce([[]])
+      .mockReturnValueOnce([[]])
+      .mockReturnValueOnce([[]])
+      .mockReturnValueOnce([[]]);
+    try {
+      const server = (await createServer()).createHttpServer({});
+      // User logging in with invalid credentials.
+      const authenticatedAgent = request.agent(server);
 
-//       response = await authenticatedAgent
-//         .post("/auth/login")
-//         .send({ username: "test", password: "test", authProvider: "SSDA" });
+      let response = await authenticatedAgent
+        .post("/auth/login")
+        .send({ username: "test", password: "wrong", authProvider: "SSDA" });
 
-//       // Expect the user to be authenticated
-//       expect(JSON.parse(response.text).success).toEqual(true);
+      // Expect the user unauthenticated status code
+      expect(response.status).toEqual(401);
 
-//       // Request the user details
-//       response = await authenticatedAgent.post("/").send({
-//         query: `query {
-//             user {
-//               givenName
-//             }
-//           }`
-//       });
-//       // Expect the request to have been successful
-//       expect(JSON.parse(response.text).data.user.givenName).toEqual("Test");
-//     } catch (e) {
-//       return;
-//     }
-//   });
+      // Expect the success field to be false
+      expect(JSON.parse(response.text).success).toEqual(false);
 
-//   it("should return an error message and status code 401 if the username or password are wrong", async () => {
-//     (ssdaAdminPool.query as any)
-//       .mockReturnValueOnce([[]])
-//       .mockReturnValueOnce([[]])
-//       .mockReturnValueOnce([[]])
-//       .mockReturnValueOnce([[]]);
-//     try {
-//       const server = (await createServer()).createHttpServer({});
-//       // User logging in with invalid credentials.
-//       const authenticatedAgent = request.agent(server);
+      // Expect the error message to be meaningful
+      expect(JSON.parse(response.text).message).toContain(
+        "username or password"
+      );
 
-//       let response = await authenticatedAgent
-//         .post("/auth/login")
-//         .send({ username: "test", password: "wrong", authProvider: "SSDA" });
+      // Request the user details
+      response = await authenticatedAgent.post("/").send({
+        query: `query { 
+            user { 
+              givenName 
+            } 
+          }`
+      });
 
-//       // Expect the user unauthenticated status code
-//       expect(response.status).toEqual(401);
+      // Expect user to be null, as the user is not authenticated any longer
+      expect(JSON.parse(response.text).data.user).toBe(null);
+    } catch (e) {
+      return;
+    }
+  });
+});
 
-//       // Expect the success field to be false
-//       expect(JSON.parse(response.text).success).toEqual(false);
+describe("/auth/logout", () => {
+  it("should log out the user who was logged in", async () => {
+    (ssdaAdminPool.query as any)
+      .mockReturnValueOnce([[{ id: 1 }]])
+      .mockReturnValueOnce([[]])
+      .mockReturnValueOnce([[{ id: 1 }]])
+      .mockReturnValueOnce([[]])
+      .mockReturnValueOnce([[{ id: 1 }]])
+      .mockReturnValueOnce([[]])
+      .mockReturnValueOnce([[{ id: 1, givenName: "Test" }]])
+      .mockReturnValueOnce([[]])
+      .mockReturnValueOnce([[]]);
 
-//       // Expect the error message to be meaningful
-//       expect(JSON.parse(response.text).message).toContain(
-//         "username or password"
-//       );
+    (bcrypt.compare as any).mockReturnValue(true);
 
-//       // Request the user details
-//       response = await authenticatedAgent.post("/").send({
-//         query: `query {
-//             user {
-//               givenName
-//             }
-//           }`
-//       });
+    // Authenticate the user
+    const agent = await createAuthenticatedAgent("test", "test", "SSDA");
 
-//       // Expect user to be null, as the user is not authenticated any longer
-//       expect(JSON.parse(response.text).data.user).toBe(null);
-//     } catch (e) {
-//       return;
-//     }
-//   });
-// });
+    // Request the user details
+    let response = await agent.post("/").send({
+      query: `query { 
+          user { 
+            givenName 
+          } 
+        }`
+    });
 
-// describe("/auth/logout", () => {
-//   it("should log out the user who was logged in", async () => {
-//     (ssdaAdminPool.query as any)
-//       .mockReturnValueOnce([[{ id: 1 }]])
-//       .mockReturnValueOnce([[]])
-//       .mockReturnValueOnce([[{ id: 1 }]])
-//       .mockReturnValueOnce([[]])
-//       .mockReturnValueOnce([[{ id: 1 }]])
-//       .mockReturnValueOnce([[]])
-//       .mockReturnValueOnce([[{ id: 1, givenName: "Test" }]])
-//       .mockReturnValueOnce([[]])
-//       .mockReturnValueOnce([[]]);
+    // Expect the request to have been successful
+    expect(JSON.parse(response.text).data.user.givenName).toEqual("Test");
 
-//     (bcrypt.compare as any).mockReturnValue(true);
+    // Log out the user
+    response = await agent.post("/auth/logout");
 
-//     // Authenticate the user
-//     const agent = await createAuthenticatedAgent("test", "test", "SSDA");
+    // Expect the user has been logged out
+    expect(JSON.parse(response.text).success).toEqual(true);
 
-//     // Request the user details
-//     let response = await agent.post("/").send({
-//       query: `query {
-//           user {
-//             givenName
-//           }
-//         }`
-//     });
+    // Request the user details
+    response = await agent.post("/").send({
+      query: `query { 
+          user { 
+            givenName 
+          } 
+        }`
+    });
 
-//     // Expect the request to have been successful
-//     expect(JSON.parse(response.text).data.user.givenName).toEqual("Test");
-
-//     // Log out the user
-//     response = await agent.post("/auth/logout");
-
-//     // Expect the user has been logged out
-//     expect(JSON.parse(response.text).success).toEqual(true);
-
-//     // Request the user details
-//     response = await agent.post("/").send({
-//       query: `query {
-//           user {
-//             givenName
-//           }
-//         }`
-//     });
-
-//     // Expect user to be null, as the user is not logged in any longer
-//     expect(JSON.parse(response.text).data.user).toBe(null);
-//   });
-// });
+    // Expect user to be null, as the user is not logged in any longer
+    expect(JSON.parse(response.text).data.user).toBe(null);
+  });
+});
