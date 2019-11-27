3d8fb341861c4184b637bd77ba45f3795a6070d5
Sifiso Myeza <myezasifiso11@gmail.com>
2019-11-27 15:30:00 +0200

    changed the read me file order

--- src/util/zipDataRequest.ts
+++ src/util/zipDataRequest.ts
@@ -113,11 +113,11 @@ export const zipDataRequest = async (
 
   // Table formatter
   const tableFormat = `
-  |${"-".repeat(nameStrLength)}|${"-".repeat(typeStrLength)}|`;
++${"-".repeat(nameStrLength)}+${"-".repeat(typeStrLength)}+`;
 
   // Table content of the table header
   const tableHeaderContent = `
-  |File name${" ".repeat(nameStrLength - "File name".length)}|Type${" ".repeat(
+|File name${" ".repeat(nameStrLength - "File name".length)}|Type${" ".repeat(
     typeStrLength - "Type".length
   )}|`;
 
@@ -129,52 +129,56 @@ export const zipDataRequest = async (
   dataFiles.forEach((file: { name: string; type: string }) => {
     // The content of the table body
     const tableBodyContent = `
-  |${file.name}${" ".repeat(nameStrLength - file.name.length)}|${
+|${file.name}${" ".repeat(nameStrLength - file.name.length)}|${
       file.type
     }${" ".repeat(typeStrLength - file.type.length)}|\r\n`;
     tableBody = tableBody + tableBodyContent;
   });
 
   // The title of the table
-  const tableTitle = `The requested files\r\n`;
+  const tableTitle = `The requested files\r\n===================\r\n`;
+
   // The table containing the data request file names and type of the product data contained by the file
-  const table = tableHeader + tableBody;
+  const table = tableHeader + tableBody + `\r\n`;
 
   // The SALT policy
   const policy = `
 Publication and acknowledgment policy
+=====================================
   
-  Publications
-  Please notify salthelp@salt.ac.za of any publication made using SALT data including
-  reviewed papers and conference proceedings.
-  
-  Science paper acknowledgements
-  All science papers that include SALT data which are submitted for publication in refereed
-  science journals must include the following words of acknowledgment:
-  
-  “All/some [choose which is appropriate] of the observations reported in this paper
-  were obtained with the Southern African Large Telescope (SALT) under program(s)
-  [insert Proposal Code(s)].”
-  
-  We recommend that the Principle Investigator is also mentioned after the Proposal Code. In
-  addition, for papers which predominantly based on SALT data, a footnote symbol should
-  appear after the paper title*, and the following text should be written as a footnote:
-  
-  *based on observations made with the Southern African Large Telescope (SALT)"
-  
-  If possible, please also include the Proposal Code and Principle Investigator in body of the
-  paper when describing observations.
-  
-  If you use data reduced by the SALT science pipeline or use the PySALT software, please
-  provide a link to http://pysalt.salt.ac.za/ and cite the following paper:
-  
-  Crawford, S.M., Still, M., Schellart, P., Balona, L., Buckley, D.A.H., Gulbis, A.A.S., Kniazev,
-  A., Kotze, M., Loaring, N., Nordsieck, K.H., Pickering, T.E., Potter, S., Romero Colmenero,
-  E., Vaisanen, P., Williams, T., Zietsman, E., 2010. PySALT: the SALT Science Pipeline.
-  SPIE Astronomical Instrumentation, 7737-82\r\n\n`;
+Publications
+------------
+Please notify salthelp@salt.ac.za of any publication made using SALT data including
+reviewed papers and conference proceedings.
+
+Science paper acknowledgements
+------------------------------
+All science papers that include SALT data which are submitted for publication in refereed
+science journals must include the following words of acknowledgment:
+
+“All/some [choose which is appropriate] of the observations reported in this paper
+were obtained with the Southern African Large Telescope (SALT) under program(s)
+[insert Proposal Code(s)].”
+
+We recommend that the Principle Investigator is also mentioned after the Proposal Code. In
+addition, for papers which predominantly based on SALT data, a footnote symbol should
+appear after the paper title*, and the following text should be written as a footnote:
+
+*based on observations made with the Southern African Large Telescope (SALT)"
+
+If possible, please also include the Proposal Code and Principle Investigator in body of the
+paper when describing observations.
+
+If you use data reduced by the SALT science pipeline or use the PySALT software, please
+provide a link to http://pysalt.salt.ac.za/ and cite the following paper:
+
+Crawford, S.M., Still, M., Schellart, P., Balona, L., Buckley, D.A.H., Gulbis, A.A.S., Kniazev,
+A., Kotze, M., Loaring, N., Nordsieck, K.H., Pickering, T.E., Potter, S., Romero Colmenero,
+E., Vaisanen, P., Williams, T., Zietsman, E., 2010. PySALT: the SALT Science Pipeline.
+SPIE Astronomical Instrumentation, 7737-82\r\n\n`;
 
   // A read me file content
-  const readMeFileContent = policy + tableTitle + table;
+  const readMeFileContent = tableTitle + table + policy;
 
   // append a file from string
   archive.append(readMeFileContent, { name: "README.txt" });
