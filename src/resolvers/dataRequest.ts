f212d7a2acdd1be23f7399fbb65cc264414d577f
Eb-Zeero <macebelene@gmail.com>
2020-03-30 16:13:35 +0200

    Reject empty data data request.

--- src/resolvers/dataRequest.ts
+++ src/resolvers/dataRequest.ts
@@ -25,6 +25,11 @@ export const createDataRequest = async (
     throw new Error("You must be logged in to create a data request");
   }
 
+  // check if there are data files
+  if (!dataFiles) {
+    throw new Error("You can not create an empty data request.");
+  }
+
   // store the requested files before adding calibrations
   const requestedDataFiles = [...dataFiles];
 
