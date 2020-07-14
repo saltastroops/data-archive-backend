ad519883313bbb3238604bd2371e218a4ddf06ce
Eb-Zeero <macebelene@gmail.com>
2020-07-14 14:20:00 +0200

    fix the data request dir missing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import fs from "fs";
 import createServer from "./createServer";
 
 const launchServer = async () => {
@@ -6,6 +7,14 @@ const launchServer = async () => {
 
   if (process.env.NODE_ENV === "test") {
     return;
+  }
+
+  // Check if the data request base directory exists
+  if (!fs.existsSync(process.env.DATA_REQUEST_BASE_DIR || "")){
+    throw new Error(
+        `The data request base directory does not exist please set environment variable 'DATA_REQUEST_BASE_DIR' to be an
+         existing directory`
+    );
   }
 
   // Start the server with CORS enabled
