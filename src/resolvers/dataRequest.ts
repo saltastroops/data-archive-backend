9a8254518a753f62b0b05bcb0e2d7b23486e6abd
Lonwabo Zaula <lonwabozaula@gmail.com>
2020-07-24 12:22:59 +0200

    code refactoring

--- src/resolvers/dataRequest.ts
+++ src/resolvers/dataRequest.ts
@@ -14,31 +14,34 @@ async function asyncForEach(array: any, callback: any) {
     await callback(array[index], index, array);
   }
 }
-
-async function data_request_limit_size(
-  fileIds: string[],
-  requestedCalibrationLevels: Set<CalibrationLevel>
-) {
+// We get the files to be downloaded by the user based on their fileIds
+async function files_to_be_downloaded(fileIds: string[]) {
   const sql = `SELECT (paths).raw,
                       (paths).reduced 
                FROM observations.artifact atf
                WHERE artifact_id = ANY($1)`;
   const res = await ssdaPool.query(sql, [fileIds]);
-  const artifacts = res.rows;
-
+  return res.rows;
+}
+// limiting the data request of the user 5GB or less, we return thr limit size in bytes
+async function data_request_limit_size(
+  fileIds: string[],
+  requestedCalibrationLevels: Set<CalibrationLevel>
+) {
   let filepath: string;
   let fileSize = 0;
-  for (const df of artifacts) {
+  const dataFiles = await files_to_be_downloaded(fileIds);
+  for (const datafile of dataFiles) {
     if (requestedCalibrationLevels.has("RAW")) {
-      filepath = process.env.FITS_BASE_DIR + df.raw;
-      fileSize += fs.statSync(filepath).size / 1000000;
+      filepath = process.env.FITS_BASE_DIR + datafile.raw;
+      fileSize += fs.statSync(filepath).size;
     }
     if (requestedCalibrationLevels.has("REDUCED")) {
-      filepath = process.env.FITS_BASE_DIR + df.reduced;
-      fileSize += fs.statSync(filepath).size / 1000000;
+      filepath = process.env.FITS_BASE_DIR + datafile.reduced;
+      fileSize += fs.statSync(filepath).size;
     }
   }
-  return Math.round(fileSize);
+  return fileSize;
 }
 export const createDataRequest = async (
   dataFiles: number[],
@@ -63,15 +66,15 @@ export const createDataRequest = async (
   if (requestedCalibrationTypes.size) {
     dataFiles = await addCalibrations(dataFiles, requestedCalibrationTypes);
   }
-
+  const MAX_SIZE = 5368709120; // 5GB
   const dataFileIdStrings = dataFiles.map(id => id.toString());
   if (
     (await data_request_limit_size(
       dataFileIdStrings,
       requestedCalibrationLevels
-    )) >= 5000
+    )) >= MAX_SIZE
   ) {
-    throw new Error("You are not allowed to to request files more than 5GB.");
+    throw new Error("The total file size for your data request exceeds 5 GB.");
   }
   const requestedDataFileIdStrings = requestedDataFiles.map(id =>
     id.toString()
